5458c27ffdf128035e0f0231878dceae39bac64e
Doughy <madeup@gmail.com>
2018-07-09 13:45:03 -0500

    Fixed Names/Made into Playground File
    Making it pretty for submitting

---
+++ Project1-SoccerLeague.playground/Contents.swift
@@ -0,0 +1,127 @@
+//: Playground - noun: a place where people can play
+
+import UIKit
+
+import Foundation
+
+let player1 = ["Name": "Joe Smith", "Height" : "42", "Soccer Exp" : "Yes", "Guardian Name(s)": "Jim and Jan Smith"]
+let player2 = ["Name": "Jill Tanner", "Height" : "36", "Soccer Exp" : "Yes", "Guardian Name(s)": "Clara Tanner"]
+let player3 = ["Name": "Bill Bon", "Height" : "43", "Soccer Exp" : "Yes", "Guardian Name(s)": "Sara and Jenny Bon"]
+let player4 = ["Name": "Eva Gordon", "Height" : "45", "Soccer Exp" : "No", "Guardian Name(s)": "Wendy and Mike Gordon"]
+let player5 = ["Name": "Matt Gill", "Height" : "40", "Soccer Exp" : "No", "Guardian Name(s)": "Charles and Sylvia Gill"]
+let player6 = ["Name": "Kimmy Stein", "Height" : "41", "Soccer Exp" : "No", "Guardian Name(s)": "Bill and Hillary Stein"]
+let player7 = ["Name": "Sammy Adams", "Height" : "45", "Soccer Exp" : "No", "Guardian Name(s)": "Jeff Adams"]
+let player8 = ["Name": "Karl Saygan", "Height" : "42", "Soccer Exp" : "Yes", "Guardian Name(s)": "Heather Bledsoe"]
+let player9 = ["Name": "Suzane Greenberg", "Height" : "44", "Soccer Exp" : "Yes", "Guardian Name(s)": "Henrietta Dumas"]
+let player10 = ["Name": "Sal Dali", "Height" : "41", "Soccer Exp" : "No", "Guardian Name(s)": "Gala Dali"]
+let player11 = ["Name": "Joe Kavalier", "Height" : "39", "Soccer Exp" : "No", "Guardian Name(s)": "Sam and Elaine Kavalier"]
+let player12 = ["Name": "Ben Finkelstein", "Height" : "44", "Soccer Exp" : "No", "Guardian Name(s)": "Aaron and Jill Finkelstein"]
+let player13 = ["Name": "Diego Soto", "Height" : "41", "Soccer Exp" : "Yes", "Guardian Name(s)": "Robin and Sarika Soto"]
+let player14 = ["Name": "Chloe Alaska", "Height" : "47", "Soccer Exp" : "No", "Guardian Name(s)": "David and Jamie Alaska"]
+let player15 = ["Name": "Arnold Willis", "Height" : "43", "Soccer Exp" : "No", "Guardian Name(s)": "Claire Willis"]
+let player16 = ["Name": "Phillip Helm", "Height" : "44", "Soccer Exp" : "Yes", "Guardian Name(s)": "Thomas Helm and Eva Jones"]
+let player17 = ["Name": "Les Clay", "Height" : "42", "Soccer Exp" : "Yes", "Guardian Name(s)": "Wynonna Brown"]
+let player18 = ["Name": "Herschel Krustofski", "Height" : "45", "Soccer Exp" : "Yes", "Guardian Name(s)": "Hyman and Rachel Krustofski"]
+
+let players = [player1, player2, player3, player4, player5, player6, player7, player8, player9, player10, player11, player12, player13, player14, player15, player16, player17, player18]
+
+
+
+var teamSharks = [[String:String]] ()
+var teamRaptors = [[String:String]] ()
+var teamDragons = [[String:String]] ()
+
+func createTeams(){
+    for item in players {
+        if item["Soccer Exp"] == "Yes"{
+            if teamSharks.count == teamDragons.count {
+                var tempItem = item
+                tempItem["team"] = "Sharks"
+                teamSharks.append(tempItem)
+                
+            }
+            else if teamRaptors.count != teamSharks.count {
+                var tempItem = item
+                tempItem["team"] = "Raptors"
+                teamRaptors.append(tempItem)
+                
+            }
+            else if teamDragons.count != teamRaptors.count {
+                var tempItem = item
+                tempItem["team"] = "Dragons"
+                teamDragons.append(tempItem)
+            }
+            else {
+                print("Not Sure How we got here")
+            }
+        }
+    }
+    for item in players {
+        if item["Soccer Exp"] == "No"{
+            if teamSharks.count == teamDragons.count {
+                var tempItem = item
+                tempItem["team"] = "Sharks"
+                teamSharks.append(tempItem)
+                
+            }
+            else if teamRaptors.count != teamSharks.count {
+                var tempItem = item
+                tempItem["team"] = "Raptors"
+                teamRaptors.append(tempItem)
+                
+            }
+            else if teamDragons.count != teamRaptors.count {
+                var tempItem = item
+                tempItem["team"] = "Dragons"
+                teamDragons.append(tempItem)
+            }
+            else {
+                print("Not Sure How we got here")
+            }
+        }
+    }
+}
+
+
+func createMessage(players:[[String:String]]) {
+    let dragonsPractice = "March 17, at 1pm"
+    let sharksPractice = "March 17, at 3pm"
+    let raptorsPractice = "March 18, at 1pm"
+    var letters = [""]
+    for player in players {
+        
+        if player["team"] == "Raptors" {
+            if player != nil {
+                
+                let playerMessage = " Dear \(player["Guardian Name(s)"]!), \n  Your child, \(player["Name"]!) is on the Raptors. They will practice on \(raptorsPractice)"
+                
+                letters.append(playerMessage)}}
+        
+        if player["team"] == "Dragons"{
+            if player != nil {
+                
+                let playerMessage = " Dear \(player["Guardian Name(s)"]!), \n Your child, \(player["Name"]!) is on the Dragons. They will practice on \(dragonsPractice)"
+                
+                letters.append(playerMessage)
+            }}
+        
+        if player["team"] == "Sharks"{
+            if player != nil {
+                
+                let playerMessage = " Dear \(player["Guardian Name(s)"]!), \n Your child, \(player["Name"]!) is on the Sharks. They will practice on \(sharksPractice)"
+                
+                letters.append(playerMessage)
+            }}
+        
+        
+    }
+    for item in letters{
+        print(item)
+        print("\n")
+    }
+}
+
+createTeams()
+createMessage(players: teamSharks)
+createMessage(players: teamRaptors)
+createMessage(players: teamDragons)
